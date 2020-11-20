a1db644bac7e72b00104c5d313ceaabdf4c6a393
mraasvel <mbraasveldt@gmail.com>
2020-11-20 10:12:49 +0100

    automatic push

--- src/convert_g.c
+++ src/convert_g.c
@@ -6,7 +6,16 @@
 /*   By: mraasvel <mraasvel@student.codam.nl>         +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2020/11/20 10:07:03 by mraasvel      #+#    #+#                 */
-/*   Updated: 2020/11/20 10:07:04 by mraasvel      ########   odam.nl         */
+/*   Updated: 2020/11/20 10:12:40 by mraasvel      ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include "ft_printf.h"
+
+int	convert_g(va_list start, t_flags flags)
+{
+	t_bits	nbr;
+	int		chars;
+
+	nbr.number = va_arg(start, double);
+}
